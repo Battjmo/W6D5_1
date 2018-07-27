6d5f077962d120d22e5960779bebfa1536e385c3
Nick Battjes <nhbattjes@gmail.com>
2018-07-27 15:29:01 -0700

    new initial commit

---
+++ frontend/widgets.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './clock';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<root/>, document.getElementByID('main'));
+});
